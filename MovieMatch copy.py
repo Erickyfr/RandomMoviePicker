1ede34f906127a15bad2867042105876504ef2a9
Ewere-byte <96740317+Erickyfr@users.noreply.github.com>
2025-04-04 12:32:52 -0500

    Added some new files

--- MovieMatch copy.py
+++ MovieMatch copy.py
@@ -5,8 +5,8 @@ import json
 Movie_history = "MovieList.json"
 User_history = "UserHistory.json"
 
-movies = [
-    {"Action": ["John Wick","Mad Max: Fury Road", "The Dark Knight", "Die Hard", "Gladiator"],
+movies = {
+    "Action": ["John Wick","Mad Max: Fury Road", "The Dark Knight", "Die Hard", "Gladiator"],
      "Comedy": ["Superbad","Step Brothers", "The Hangover", "Dumb and Dumber", "Monty Python and the Holy Grail"],
      "Drama" : ["The Shawshank Redemption", "Forrest Gump", "Fight Club", "The Godfather", "Parasite"],
      "Sci-Fi": ["Inception", "The Matrix", "Interstellar", "Blade Runner 2049", "Ex Machina"],
@@ -14,7 +14,7 @@ movies = [
      "Thriller": ["Se7en", "Gone Girl", "Prisoners", "Zodiac", "No Country for Old Men"],
      "Animation": ["Spirited Away", "Toy Story", "Finding Nemo", "The Lion King", "Inside Out"]
      }
-]
+
     
 def SaveMovieHistory(Movie_history, movie):
     with open(Movie_history, "w") as file:
@@ -24,7 +24,7 @@ def LoadMovieHistory(Movie_history):
     if os.path.exists(Movie_history):
         with open(Movie_history, "r") as file:
             return json.load(file)
-    return []
+    return [movies]
 
 def SaveUserHistory(User_history,user_data):
     with open(User_history, "w") as userfile:
@@ -45,7 +45,7 @@ def CheckUserHistory(name, user_data):
         print(f"Hello {name}, Welcome to MovieMatch!, Here is a movie suggestion for you.")
     else:
         last_movie = user_data[name]["suggested_movie"][-1]["movie"]
-        print(f"Welcome back {name}!, The last movie suggested to you was: {last_movie}.")
+        print(f"Welcome back {name}!, The last movie suggested to you was: {last_movie}. \n")
         
 def RandomizeMovie(movie):
     random_movies = random.choice(movie)
@@ -57,44 +57,53 @@ def UserAddMovie(movie, name):
     movie_request = input(f"{name}, would you like to add a movie? (yes/no)- ")
     if movie_request.strip().lower() == "yes":
         genre_check = input("please enter the genre you want to add a movie to: ")
-    
-        if genre_check.title() in movie[0]:
-            print(f"{name}, you have selected the {genre_check} genre.")
-            print(f"{name}, Just to let you know, be curious of spelling and capitalization, for the movie you are adding.")
+        print("\n")
+        
+        if genre_check.strip().title() in movie[0]:
+            print(f"{name}, you have selected the {genre_check} genre. \n")
+            print(f"{name}, Just to let you know, be curious of spelling and capitalization, for the movie you are adding. \n")
             new_movie = input(f"Enter the name of the movie you want to add in {genre_check}, {name}: ").title()
+            
          #checking for duplicates
             if new_movie in movie[0][genre_check.title()]:  
-                print(f"{name}, Looks like {new_movie} is already in the {genre_check.title()} genre. Try again.")
+                print(f"{name}, Looks like {new_movie} is already in the {genre_check.title()} genre. Try again. \n")
                 return None, None
             else:
                 movie[0][genre_check.title()].append(new_movie)
-                print(f"{new_movie} has been added to the {genre_check.title()} genre.")
+                print(f"{new_movie} has been added to the {genre_check.title()} genre. \n")
                 return genre_check, new_movie
                 
         else:
-            print("Looks like we do not have the genre you are looking for")
-    else:
-        return None, None
+            print("Sorry, Looks like we do not have the genre you are looking for. \n")
         
+    return None, None
 
 def main():
     movie_data = LoadMovieHistory(Movie_history)
     user_data = LoadUserHistory(User_history)
+    
+    print("="*30)
+    
     name = input("Enter your name: ").strip().capitalize()
     CheckUserHistory(name, user_data)
     genre, selected_movie = RandomizeMovie(movie_data)
     
+    #saving user history
     user_data[name]["suggested_movie"].append({"movie": selected_movie, "genre": genre})
     
+    #Calling a function
     genre_check, new_movie = UserAddMovie(movie_data, name)
+    
+    # Save newly added movie to both user history and main database    
     if new_movie:
         user_data[name]["added_movie"].append({"movie": new_movie, "genre": genre_check})
         SaveMovieHistory(Movie_history, movie_data)
     
     SaveUserHistory(User_history, user_data)
     
+    
     print(f"{name}, your movie suggestion is: {selected_movie} in the {genre} genre. Hope you enjoy it!!")
-    print(f"Thank you {name} for using MovieMatch! Your history has been saved.")
+    print(f"Thank you {name} for using MovieMatch! Your history has been saved. \n")
     
 
 if __name__ == "__main__":
